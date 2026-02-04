407f68597546b2b2b95a20b6e1761d1f7baeb072
lyellasiri001 <91278036+lyellasiri001@users.noreply.github.com>
2026-02-04 13:24:08 -0800

    Add files via upload

---
+++ ULN2003_Driver/ULN2003.h
@@ -0,0 +1,34 @@
+/* 
+ * Function prototypes for the DRV8424/25 stepper motor driver. Defined here are the functions
+ * I would expect to be available for a programmer to call to interface with the driver. T
+ *
+ *
+ */
+#pragma once
+#include <stdint.h>
+#include "stm32g4xx_hal.h" // i think this is the correct file but check when you initialize a project
+
+typedef struct {
+	TIM_HandleTypeDef *htim_step;
+	uint32_t tim_step_channel;
+	GPIO_TypeDef *IN1_port;
+	uint16_t IN1_pin;
+    GPIO_TypeDef *IN2_port;
+	uint16_t IN2_pin;
+    GPIO_TypeDef *IN3_port;
+	uint16_t IN3_pin;
+    GPIO_TypeDef *IN4_port;
+	uint16_t IN4_pin;	
+} ULN2003_Handle;
+
+
+// Initializes handle for the driver that holds  all hardware details (pwm channel, pin assignments).
+void ULN2003_Init(DRV8425_Handle *hdrv);
+
+// Drive the motor a given number of steps with the direction indicated by the sign of param steps, at the given frequency. 
+uint8_t ULN2003_DriveSteps(DRV8425_Handle *hdrv, int32_t steps, uint32_t freq); 
+
+// Stop motor regardless of steps remaining.
+uint8_t ULN2003_Stop(DRV8425_Handle *hdrv);
+
+// Those are the most important. After that is implemented and tested, consider adding more like sleep mode, microstepping modes, current limiting, and more.
